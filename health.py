4775e639abca5b229ae5b9e5660626c8406c9730
adil-zhang <55967755+adil-zhang@users.noreply.github.com>
2021-02-24 10:07:07 +0800

    Update health.py

--- health.py
+++ health.py
@@ -7,20 +7,17 @@ import requests
 
 import  base64
 
-url = "aHR0cDovLzEwLjEwNS4xMS4yMjo4MTMzL2hlYWx0aC9zYXZlL2Z1Y2V6aGFuZ3NoaWRpQGZvdG9u"
-url = base64.b64decode(url).decode("utf-8")
+url = "https://health.foton.com.cn/health-attendance/health/save/fucezhangshidi@foton"
+url1 = "https://health.foton.com.cn/health-attendance/health/save/fucelili@foton"
 
-url1 = "aHR0cDovLzEwLjEwNS4xMS4yMjo4MTMzL2hlYWx0aC9zYXZlL2Z1Y2VsaWxpQGZvdG9u"
-url1 = base64.b64decode(url1).decode("utf-8")
-
-url2 = "http://10.105.11.22:8133/health/save/jingguodong@foton"
-url3 = "http://10.105.11.22:8133/health/save/kangchao3@foton"
-url4 = "http://10.105.11.22:8133/health/save/wangpengtao2@foton"
-url5 = "http://10.105.11.22:8133/health/save/bjfcjinlei@foton"
-url6 = "http://10.105.11.22:8133/health/save/donglulu@foton"
-url7 = "http://10.105.11.22:8133/health/save/fuceyujianbo@foton"
-url8 = "http://10.105.11.22:8133/health/save/huxinxin@foton"
-url9 = "http://10.105.11.22:8133/health/save/fucelixiao@foton"
+url2 = "https://health.foton.com.cn/health-attendance/health/save/jingguodong@foton"
+url3 = "https://health.foton.com.cn/health-attendance/health/save/kangchao3@foton"
+url4 = "https://health.foton.com.cn/health-attendance/health/save/wangpengtao2@foton"
+url5 = "https://health.foton.com.cn/health-attendance/health/save/bjfcjinlei@foton"
+url6 = "https://health.foton.com.cn/health-attendance/health/save/donglulu@foton"
+url7 = "https://health.foton.com.cn/health-attendance/health/save/fuceyujianbo@foton"
+url8 = "https://health.foton.com.cn/health-attendance/health/save/huxinxin@foton"
+url9 = "https://health.foton.com.cn/health-attendance/health/save/fucelixiao@foton"
 values={"province":"北京","city":"北京市","addressType":"整租房","temperature":"36.7","dayNum":"","contactHbPerplr":"无接触","toWh":"未去过/路过武汉","familySymptom":"无症状","remarks":"","otherDesc":"","backDate":"2020-02-12","jtgj":"自驾/步行","bc":"","addressOther":"","hbOther":"","familyOther":None,"lj":"是","ljOther":"","workStatus":"到岗上班","workOther":"","returnCountry":"未出国","returnCountryRemarks":"","provinceId":"110000","symptom":"无症状"}
 values_json=json.dumps(values)
 headers = {'content-type': 'application/json'}
