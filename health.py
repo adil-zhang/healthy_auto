482dcc31f23b5d416fbe8b1e8ed702c43c5b7f96
Adil <adil_zhang@163.com>
2021-03-03 16:40:40 +0800

    adil
    调整将用户隐藏

--- health.py
+++ health.py
@@ -6,15 +6,22 @@ import json
 
 import requests
 
-#人员名单
-name = ['fucezhangshidi','fucelili','bjfcjinlei','donglulu','fuceyujianbo','fucelixiao','huxinxin']
+def auto_health():
+
 #请求内容转json
 values={"province":"北京","city":"北京市","addressType":"整租房","temperature":"36.7","dayNum":"","contactHbPerplr":"无接触","toWh":"未去过/路过武汉","familySymptom":"无症状","remarks":"","otherDesc":"","backDate":"2020-02-12","jtgj":"自驾/步行","bc":"","addressOther":"","hbOther":"","familyOther":None,"lj":"是","ljOther":"","workStatus":"到岗上班","workOther":"","returnCountry":"未出国","returnCountryRemarks":"","provinceId":"110000","symptom":"无症状"}
 values_json=json.dumps(values)
 #请求头
 headers = {'content-type': 'application/json'}
+#多人循环
 
-#人太多了，写个循环
 for name in name:
     url='https://health.foton.com.cn/health-attendance/health/save/'+name+'@foton'
     req = requests.post(url,data=values_json,headers=headers)
+    return(name + '今日打卡成功！')
+
+
+if __name__ == '__main__':
+    
+    auto=auto_health(name)
+    
