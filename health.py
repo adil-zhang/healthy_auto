57e4ce89cf369f5abc1b08e9ca3238b0aff4e7c5
Adil <adil_zhang@163.com>
2021-03-03 17:36:22 +0800

    Update health.py

--- health.py
+++ health.py
@@ -31,7 +31,9 @@ if __name__ == '__main__':
 
     info=getarg()
     name=info['name']
+    print(name)
     head=gethead()
+
     for name in name:
         url = 'https://health.foton.com.cn/health-attendance/health/save/'+name+'@foton'
         req = requests.post(url, data=head.values_json, headers=head.headers)
